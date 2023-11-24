8ca0e61b1d0d0283975ca2141c8d1db1653271a3
Khatskelevich Aleksey Yuryevich <akhatskelevich@ozon.ru>
2023-11-24 23:37:10 +0300

    feat: подключен element-ui

---
+++ src/components/layout/BaseHeader.vue
@@ -0,0 +1,31 @@
+<script setup>
+const emit = defineEmits(['pageChosen'])
+
+const goTo = (pageIndex) => {
+    emit('pageChosen', pageIndex)
+}
+</script>
+
+<template>
+    <el-menu class="el-menu-demo" mode="horizontal">
+        <div class="logo-container">
+            <img class="logo" src="rosatomlogo.png">
+        </div>
+        <el-menu-item index="1" @click="goTo(1)">Временная шкала</el-menu-item>
+        <el-menu-item index="2" @click="goTo(2)">Загрузить данные</el-menu-item>
+        <el-menu-item index="3" @click="goTo(3)">Дашборд</el-menu-item>
+    </el-menu>
+</template>
+
+<style scoped>
+.logo-container {
+    display: flex;
+    align-items: center;
+    margin: 0 20px;
+}
+
+.logo {
+    height: 20px;
+    line-height: 59px;
+}
+</style>
