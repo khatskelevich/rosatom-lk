7335a2fc5c3e5727931dcf03b7d0c3abd9c89b73
Khatskelevich Aleksey Yuryevich <akhatskelevich@ozon.ru>
2023-11-25 02:35:18 +0300

    feat: добавлен роутинг

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <script setup>
+import {ref} from 'vue'
 import BaseHeader from "./components/layout/BaseHeader.vue";
 import GantChart from "./components/GantChart.vue";
+import TimelineChart from "./components/TimelineChart.vue";
+import UploadForm from "./components/UploadForm.vue";
 
+let currentIndex = ref(0)
 const changePage = (pageIndex) => {
-
+    currentIndex.value = pageIndex
 }
+
+const components = [TimelineChart, UploadForm]
 </script>
 
 <template>
-    <BaseHeader @page-chosen="changePage"/>
+    <el-header>
+        <BaseHeader
+                @page-chosen="changePage"
+                :current-page="currentIndex"
+        />
+    </el-header>
 
-    <GantChart/>
+    <el-main>
+        <component :is="components[currentIndex]"></component>
+    </el-main>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
 }
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+    filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- src/components/layout/BaseHeader.vue
+++ src/components/layout/BaseHeader.vue
@@ -1,5 +1,6 @@
 <script setup>
 const emit = defineEmits(['pageChosen'])
+const props = defineProps(['currentPage'])
 
 const goTo = (pageIndex) => {
     emit('pageChosen', pageIndex)
@@ -7,13 +8,17 @@ const goTo = (pageIndex) => {
 </script>
 
 <template>
-    <el-menu class="el-menu-demo" mode="horizontal">
+    <el-menu
+        class="el-menu-demo"
+        mode="horizontal"
+        :default-active="1"
+    >
         <div class="logo-container">
             <img class="logo" src="rosatomlogo.png">
         </div>
-        <el-menu-item index="1" @click="goTo(1)">Временная шкала</el-menu-item>
-        <el-menu-item index="2" @click="goTo(2)">Загрузить данные</el-menu-item>
-        <el-menu-item index="3" @click="goTo(3)">Дашборд</el-menu-item>
+        <el-menu-item index="0" @click="goTo(0)">Временная шкала</el-menu-item>
+        <el-menu-item index="1" @click="goTo(1)">Загрузить данные</el-menu-item>
+        <el-menu-item index="2" @click="goTo(2)">Дашборд</el-menu-item>
     </el-menu>
 </template>
 
