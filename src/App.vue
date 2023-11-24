58b23a6ee6b7271bce6617de720a573d260ea134
Khatskelevich Aleksey Yuryevich <akhatskelevich@ozon.ru>
2023-11-25 00:21:08 +0300

    feat: создан прототип диаграммы ганта

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseHeader from "./components/layout/BaseHeader.vue";
+import GantChart from "./components/GantChart.vue";
+
+const changePage = (pageIndex) => {
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <BaseHeader @page-chosen="changePage"/>
+
+    <GantChart/>
 </template>
 
 <style scoped>
